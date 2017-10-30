472ddc03a9d9f3dee7b48d5a4ce9126a1a58dac4
iraj <iraj.jelo@gmail.com>
2017-10-30 18:06:26 +0330

    added some options

--- options/options.js
+++ options/options.js
@@ -1,31 +1,35 @@
-var ids = {mouseButtonSelect:"#mouseButtonSelect",
+var ids = {
+  notificationCheckbox: "#notification",
+  reverseCheckbox: "#reverse",
+  clickSelect:"#clickSelect",
   selectionKeySelect:"#selectionKeySelect",
-  linksSelect:"#linksSelect"}
-/*
-var mouse_buttons = {'Left': 'left',
-'Middle': 'middle', 
-'Right': 'right'};
-*/
+  linksSelect:"#linksSelect"
+}
+
+var mouse_buttons = {'Left': 'left', 'Middle': 'middle', 'Right': 'right'};
+
 var keys = {'<Ctrl>|<Alt>|<Shift>': 'CtrlAltShift',
   '<Ctrl>':'Ctrl', 
   '<Alt>':'Alt',
   '<Shift>':'Shift',
    'None': 'None'};
 
-var links_status = {'clickable': 'clickable', 'non-clickable': 'non-clickable'};
+var links_status = {'Clickable': 'clickable', 'Non-clickable': 'non-clickable'};
 
-/*var mouse_button_select = document.querySelector("#mouseButtonSelect");*/
-var key_select = document.querySelector("#selectionKeySelect");
-var links_select = document.querySelector("#linksSelect");
+var notification_checkbox = document.querySelector(ids.notificationCheckbox)
+var reverse_checkbox = document.querySelector(ids.reverseCheckbox);
+var mouse_button_select = document.querySelector(ids.clickSelect)
+var key_select = document.querySelector(ids.selectionKeySelect);
+var links_select = document.querySelector(ids.linksSelect);
 
-/*
+
 for (button in mouse_buttons){
   var option = document.createElement('option');
   option.innerText = button;
   option.value = mouse_buttons[button];
-  document.querySelector(ids.mouseButtonSelect).append(option);
+  document.querySelector(ids.clickSelect).append(option);
 }
-*/
+
 for (key in keys){
   var option = document.createElement('option');
   option.innerText = key;
@@ -43,16 +47,22 @@ for (link_status in links_status){
 function saveOptions(e) {
   e.preventDefault();
 
-  /*var mouse_button_selected = [mouse_button_select.selectedOptions[0].innerHTML, mouse_button_select.selectedIndex];*/
+  var notification_checked = notification_checkbox.checked;
+
+  var reverse_checked = reverse_checkbox.checked;
+
+  var click_selected = [mouse_button_select.selectedOptions[0].innerHTML, mouse_button_select.selectedIndex];
 
   var key_selected = [key_select.selectedOptions[0].innerText, key_select.selectedIndex];
 
   var links_selected = [links_select.selectedOptions[0].innerText, links_select.selectedIndex];
 
   browser.storage.local.set({
-    //mouse_button: mouse_button_selected,
-    key: key_selected,
-    links: links_selected
+    "notification": notification_checked,
+    "reverse": reverse_checked,
+    "mouse_button": click_selected,
+    "key": key_selected,
+    "links": links_selected
   });
   alert("successfully saved.");
 }
@@ -60,7 +70,9 @@ function saveOptions(e) {
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    //mouse_button_select.selectedIndex = (result.mouse_button == undefined)? "0" : result.mouse_button[1];
+    notification_checkbox.checked = result.notification;
+    reverse_checkbox.checked = result.reverse;
+    mouse_button_select.selectedIndex = (result.mouse_button == undefined)? "0" : result.mouse_button[1];
     key_select.selectedIndex = (result.key == undefined)? 0 : result.key[1];
     links_select.selectedIndex = (result.links == undefined)? 1 : result.links[1];
   }
@@ -69,7 +81,7 @@ function restoreOptions() {
     console.log(`Error: ${error}`);
   }
 
-  var getting = browser.storage.local.get(["key", "links"]);
+  var getting = browser.storage.local.get(["notification", "reverse", "mouse_button", "key", "links"]);
   getting.then(setCurrentChoice, onError);
 
 }
